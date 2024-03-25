8f133d28dab119cbccf6860a0385f4bdc3a3214a
Ravil Berish <quadraticteam@gmail.com>
2024-03-25 18:19:36 +0300

    jsx-runtime

--- src/jsx-runtime/index.ts
+++ src/jsx-runtime/index.ts
@@ -1,9 +1,40 @@
-// Тег может быть как строкой, так и функцией – если парсим
-
 import { createElement } from '../ReflowElement';
-import { Attributes, FunctionComponent, ReflowNode } from '../types';
+import { FunctionComponent, ReflowElement, ReflowNode } from '../types';
 
-export function h<P extends {}>(type: FunctionComponent<P>, props?: (Attributes & P) | null, ...children: ReflowNode[]) {
-  if (typeof type === 'function') return createElement(type, props, ...children);
-  throw new TypeError('Reflow not supported custom tags.');
+namespace JSX {
+  export type HTMLAttributes = Record<string, JSXNode | undefined> & JSXChildren;
+
+  // Allow any HTML tag
+  export type IntrinsicElements = Record<string, HTMLAttributes>;
+
+  // Declare the shape of JSX rendering result
+  // This is required so the return types of components can be inferred
+  export type Element = any;
 }
+
+// Export the main namespace
+export { JSX };
+
+export function renderJSX(tag: FunctionComponent, props: Record<string, any>, _key?: string): JSX.Element {
+  if (typeof tag === 'function') {
+    // handling Function Components
+    return createElement(tag, props);
+  }
+
+  throw new TypeError('Custom tags not supported.');
+}
+
+// Export factories
+export const jsx = renderJSX;
+export const jsxs = renderJSX;
+export const jsxDEV = renderJSX;
+
+export class RenderedNode {
+  public constructor(public readonly string: string) {}
+}
+
+export interface JSXChildren {
+  children?: JSXNode | JSXNode[] | undefined;
+}
+
+export type JSXNode = RenderedNode | (() => JSXNode) | boolean | null | undefined;
